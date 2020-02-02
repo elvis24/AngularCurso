e8f8de089bc47e403347a4c5e26e9d94317b4b93
Elvis Villaca <elvis.villaca@skf.com>
2020-02-02 14:07:59 -0500

    primer cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+//routes
+
+//Componentes
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/shared/navbar/navbar.component';
+import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
+import {HeroesComponent} from './components/heroes/heroes.component';
+import {from} from 'rxjs';
+import {AppRoutingModule} from './app-routing.module';
+import {HeroesService} from './servicios/heroes.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
